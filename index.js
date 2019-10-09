e84be02c2c55f992e3b3999ee518abb36a4beb4b
TatiaZh <38917757+TatiaZh@users.noreply.github.com>
2019-10-09 23:07:09 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,67 +1,67 @@
 'use strict';
 
-import React, { Component } from 'react'
-import { View, NativeMethodsMixin, Dimensions } from 'react-native'
+import React, { Component } from 'react';
+import { View, NativeMethodsMixin, Dimensions } from 'react-native';
 
-exports.InViewPort = class extends Component {
+export default class InViewPort extends Component {
   constructor(props) {
-    super(props)
-    this.state = { rectTop: 0, rectBottom: 0 }
+    super(props);
+    this.state = { rectTop: 0, rectBottom: 0 };
   }
 
   componentDidMount() {
     if (!this.props.disabled) {
-      this.startWatching()
+      this.startWatching();
     }
   }
 
   componentWillUnmount() {
-    this.stopWatching()
+    this.stopWatching();
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.disabled) {
-      this.stopWatching()
+      this.stopWatching();
     } else {
-      this.lastValue = null
-      this.startWatching()
+      this.lastValue = null;
+      this.startWatching();
     }
   }
 
   startWatching() {
     if (this.interval) {
-      return
+      return;
     }
     this.interval = setInterval(() => {
       if (!this.myview) {
-        return
+        return;
       }
       this.myview.measure((x, y, width, height, pageX, pageY) => {
         this.setState({
           rectTop: pageY,
-          rectBottom: pageY + height,
+          rectBottom: pageY + height + 100,
           rectWidth: pageX + width
-        })
-      })
-      this.isInViewPort()
-    }, this.props.delay || 100)
+        });
+      });
+      this.isInViewPort();
+    }, this.props.delay || 100);
   }
 
   stopWatching() {
-    this.interval = clearInterval(this.interval)
+    this.interval = clearInterval(this.interval);
   }
 
   isInViewPort() {
-    const window = Dimensions.get('window')
+    const window = Dimensions.get('window');
     const isVisible =
       this.state.rectBottom != 0 &&
       this.state.rectTop >= 0 &&
       this.state.rectBottom <= window.height &&
       this.state.rectWidth > 0 &&
-      this.state.rectWidth <= window.width
+      this.state.rectWidth <= window.width;
     if (this.lastValue !== isVisible) {
-      this.lastValue = isVisible
-      this.props.onChange(isVisible)
+      this.lastValue = isVisible;
+      this.props.onChange(isVisible);
     }
   }
 
@@ -69,13 +69,13 @@ exports.InViewPort = class extends Component {
     return (
       <View
         collapsable={false}
-        ref={component => {
-          this.myview = component
+        ref={(component) => {
+          this.myview = component;
         }}
         {...this.props}
       >
         {this.props.children}
       </View>
-    )
+    );
   }
 }
